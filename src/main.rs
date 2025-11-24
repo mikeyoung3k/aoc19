f720a3897d40ff3c8dc6852ec962b8d49e7492b1
Mike Young <mikeyoung3k@gmail.com>
2025-11-24 06:55:07 +0000

    Don't run every day

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ mod intcomp;
 const BASE_DIR: &'static str = "C://Users/Mike_/Documents/Rust/advent_of_code/aoc2019/data";
 
 fn main() {
-    let (r1,r2) = day1::run();
-    println!("Day 1 part 1: {}, Day 1 part 2: {}", r1, r2);
-    let (r1,r2) = day2::run();
-    println!("Day 2 part 1: {}, Day 2 part 2: {}", r1, r2);
-    let (r1,r2) = day3::run();
-    println!("Day 3 part 1: {}, Day 3 part 2: {}", r1, r2);
-    let (r1,r2) = day4::run();
-    println!("Day 4 part 1: {}, Day 4 part 2: {}", r1, r2);
-    let (r1,r2) = day5::run();
-    println!("Day 5 part 1: {}, Day 5 part 2: {}", r1, r2);
+    // let (r1,r2) = day1::run();
+    // println!("Day 1 part 1: {}, Day 1 part 2: {}", r1, r2);
+    // let (r1,r2) = day2::run();
+    // println!("Day 2 part 1: {}, Day 2 part 2: {}", r1, r2);
+    // let (r1,r2) = day3::run();
+    // println!("Day 3 part 1: {}, Day 3 part 2: {}", r1, r2);
+    // let (r1,r2) = day4::run();
+    // println!("Day 4 part 1: {}, Day 4 part 2: {}", r1, r2);
+    // let (r1,r2) = day5::run();
+    // println!("Day 5 part 1: {}, Day 5 part 2: {}", r1, r2);
 }
