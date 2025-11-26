abf7c9b986624572786ee00283e04c164a1c262f
Mike Young <mikeyoung3k@gmail.com>
2025-11-26 07:37:56 +0000

    Day 6 pt 1

--- src/day6.rs
+++ src/day6.rs
@@ -6,12 +6,12 @@ use crate::BASE_DIR;
 pub fn run() -> (isize, isize) {
     let path = Path::new(BASE_DIR).join("real").join("day6.txt");
     let input_string = read_to_string(path).expect("Error reading file");
-    let test_data = OrbitNode::from_string(input_string);
-    (0,0)
+    let head = OrbitNode::from_string(input_string);
+    (pt1(&head),0)
 }
 
 fn pt1(head: &OrbitNode) -> isize {
-    0
+    head.count_children(1)
 }
 
 fn pt2() -> isize {
@@ -26,38 +26,55 @@ struct OrbitNode {
 
 impl OrbitNode {
     fn from_string(input: String) -> OrbitNode {
+        let mut orphan_nodes: Vec<OrbitNode> = vec![];
         // Return the head node, which will own all children nodes
         let mut orbits = input.lines();
-        let mut parts = orbits.next().unwrap().split(")").map(|s| s.to_owned());
-        let id = parts.next().unwrap();
-        let child_id = parts.next().unwrap();
-        let child = OrbitNode{
-            id: child_id,
-            children: None,
-        };
+
 
         let mut head = OrbitNode{
-            id,
-            children: Some(vec![child]),
+            id: "".to_string(),
+            children: None
         };
 
-        while let Some(orbit) = orbits.next() {
+        'outer: while let Some(orbit) = orbits.next() {
+            // Check if child exists in orphan nodes and push. If not, create and push
             let mut parts = orbit.split(")").map(|s| s.to_owned());
             let id = parts.next().unwrap();
             let child_id = parts.next().unwrap();
-            let child = OrbitNode{
-                id: child_id,
-                children: None,
+            // Before creating a child - see if child already exists in the orphan nodes
+            let child = if let Some(found_child) = orphan_nodes.extract_if(..,|n| &n.id == &child_id).next() {
+                found_child
+            } else {
+                OrbitNode{
+                    id: child_id,
+                    children: None,
+                }
             };
+
+            // Searching main tree for child
             if let Some(found) = head.get_child_node(&id){
                 found.insert_child(child);
-            }
-            else {
-                panic!("Owning node not found for {}", child.id);
+            } else {
+                for node in &mut orphan_nodes {
+                    if let Some(parent) = node.get_child_node(&id){
+                        parent.insert_child(child);
+                        // If found, continue to next orbit line
+                        continue 'outer;
+                    }
+                }
+                // If we don't find it, create and push to orphan nodes
+                let parent = OrbitNode{
+                    id,
+                    children: Some(vec![child]),
+                };
+                // Or - if we've found COM - make that head
+                if &parent.id == "COM" {
+                    head = parent
+                } else {
+                    orphan_nodes.push(parent);
+                }
             }
         }
-
-
         head
     }
 
@@ -85,6 +102,17 @@ impl OrbitNode {
         } else {
             self.children = Some(vec![child]);
         }
+    }
+
+    fn count_children(&self,level:isize) -> isize {
+        let mut count = 0;
+        if let Some(children) = &self.children {
+            for child in children {
+                count += level;
+                count += child.count_children(level + 1);
+            }
+        }
+        count
     }
 }
 
