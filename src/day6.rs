b7c588f94df8fca8166144efb319ea26898e7398
Mike Young <mikeyoung3k@gmail.com>
2025-11-26 20:15:04 +0000

    Allow dead code everywhere. Day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -7,16 +7,24 @@ pub fn run() -> (isize, isize) {
     let path = Path::new(BASE_DIR).join("real").join("day6.txt");
     let input_string = read_to_string(path).expect("Error reading file");
     let head = OrbitNode::from_string(input_string);
-    (pt1(&head),0)
+    (pt1(&head),pt2(&head))
 }
 
 fn pt1(head: &OrbitNode) -> isize {
     head.count_children(1)
 }
 
-fn pt2() -> isize {
-    0
+fn pt2(head: &OrbitNode) -> isize {
+    let mut you: Vec<&str> = vec![];
+    let mut san: Vec<&str> = vec![];
+    head.find_parents("YOU", &mut you);
+    head.find_parents("SAN", &mut san);
+    let you_count = you.iter().fold(0,|acc, s| if !san.contains(s) {acc + 1} else {acc}) - 1;
+    let san_count = san.iter().fold(0,|acc, s| if !you.contains(s) {acc + 1} else {acc}) - 1;
+    you_count+san_count
+    
 }
+
 
 #[derive(Debug, PartialEq, Eq)]
 struct OrbitNode {
@@ -114,6 +122,23 @@ impl OrbitNode {
         }
         count
     }
+
+    fn find_parents<'a>(&'a self, target: &str, result: &mut Vec<&'a str>) {
+        if self.id == target {
+            result.push(&self.id);
+            return
+        }
+        
+        if let Some(children) = &self.children {
+            for child in children {
+                child.find_parents(target,result);
+                if !result.is_empty() {
+                    result.push(&self.id);
+                    return
+                }
+            }
+        }
+    }
 }
 
 
@@ -130,6 +155,14 @@ mod test {
     }
 
     #[test]
+    fn test_pt2() {
+        let test_path = Path::new(BASE_DIR).join("test").join("day6-pt2.txt");
+        let test_input_string = read_to_string(test_path).expect("Error reading file");
+        let test_data = OrbitNode::from_string(test_input_string);
+        assert_eq!(pt2(&test_data), 4);
+    }
+
+    #[test]
     fn test_map_build() {
         // COM - B - C
         //         - D -E
@@ -141,7 +174,18 @@ mod test {
         let node_c = OrbitNode{id: "C".to_string(), children: None};
         let node_b = OrbitNode{id: "B".to_string(), children: Some(vec![node_c, node_d])};
         let node_com = OrbitNode{id: "COM".to_string(), children: Some(vec![node_b])};
-
+        
         assert_eq!(test_data, node_com);
+    }
+    
+    #[test]
+    fn test_find_parents() {
+        let test_path = Path::new(BASE_DIR).join("test").join("day6-pt2.txt");
+        let test_input_string = read_to_string(test_path).expect("Error reading file");
+        let test_data = OrbitNode::from_string(test_input_string);
+        let mut res = vec![];
+        test_data.find_parents("YOU",&mut res);
+        let expect = vec!["YOU","K","J","E","D","C","B","COM"];
+        assert_eq!(expect,res);
     }
 }
